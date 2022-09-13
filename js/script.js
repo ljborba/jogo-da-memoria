e23e14b1d30834b9dd8b29f7e0eaaed082057edc
Lucas Janing <janinglucas123@gmail.com>
2022-09-13 20:47:30 -0300

    1.4

--- js/script.js
+++ js/script.js
@@ -11,3 +11,29 @@ let techs = ["bootstrap",
     "mongo",
     "node",
     "react"];
+
+createCardsFromTechs(techs);
+
+function createCardsFromTechs(techs) {
+    let cards = [];
+        for(let tech of techs) {
+            cards.push(createPairFromTech(tech));
+        };
+    return cards.flatMap(pair => pair);
+};
+
+function createPairFromTech(tech) {
+    return [{
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }, {
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false,
+    }]
+};
+
+function createIdWithTech(tech) {
+    return tech + parseInt(Math.random() * 1000);
+}
