1a3236a95a53d404e145d678ff2befe2ec443bf9
Lucas Janing <janinglucas123@gmail.com>
2022-09-14 20:42:21 -0300

    1.8

--- js/script.js
+++ js/script.js
@@ -3,54 +3,15 @@ const BACK = "card_back"
 const CARD = "card"
 const ICON = "icon"
 
-let techs = ["bootstrap",
-    "css",
-    "electron",
-    "firebase",
-    "html",
-    "javascript",
-    "jquery",
-    "mongo",
-    "node",
-    "react"];
-
-let cards = null;
-
     starGame();
 
-function createCardsFromTechs(techs) {
-    let cards = [];
-        techs.forEach((tech) => {
-            cards.push(createPairFromTech(tech));
-        });
-    return cards.flatMap(pair => pair);
-};
-
-function createPairFromTech(tech) {
-    return [{
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }, {
-        id: createIdWithTech(tech),
-        icon: tech,
-        flipped: false,
-    }];
-};
-
-function createIdWithTech(tech) {
-    return tech + parseInt(Math.random() * 1000);
-};
-
 function starGame() {
-    cards = createCardsFromTechs(techs);
-    shuffleCards(cards);
-    initializeCards(cards);
+    initializeCards(game.createCardsFromTechs());
 };
 
 function initializeCards(cards) {
     let gameBoard = document.getElementById("gameBoard");
-        cards.forEach(card => {
+        game.cards.forEach(card => {
             let cardElement = document.createElement("div");
             cardElement.id = card.id;
             cardElement.classList.add(CARD);
@@ -59,17 +20,6 @@ function initializeCards(cards) {
             cardElement.addEventListener("click", flipCard);
             gameBoard.appendChild(cardElement);
         });
-};
-
-function shuffleCards(cards) {
-    let currentIndex = cards.length;
-    let randomIndex = 0;
-
-        while(currentIndex !== 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-            [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
-        };
 };
 
 function flipCard() {
