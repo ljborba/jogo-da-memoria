5a3a71cb0eadf3a6a2b3c8435530b50191be43a3
Lucas Janing <janinglucas123@gmail.com>
2022-09-13 21:24:15 -0300

    1.6

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front"
 const BACK = "card_back"
+const CARD = "card"
+const ICON = "icon"
 
 let techs = ["bootstrap",
     "css",
@@ -18,9 +20,9 @@ let cards = null;
 
 function createCardsFromTechs(techs) {
     let cards = [];
-        for(let tech of techs) {
+        techs.forEach((tech) => {
             cards.push(createPairFromTech(tech));
-        };
+        });
     return cards.flatMap(pair => pair);
 };
 
@@ -43,7 +45,20 @@ function createIdWithTech(tech) {
 function starGame() {
     cards = createCardsFromTechs(techs);
     shuffleCards(cards);
-    console.log(cards);
+    initializeCards(cards);
+};
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+        cards.forEach(card => {
+            let cardElement = document.createElement("div");
+            cardElement.id = card.id;
+            cardElement.classList.add(CARD);
+            cardElement.dataset.icon = card.icon;
+            createCardContent(card, cardElement);
+            cardElement.addEventListener("click", flipCard);
+            gameBoard.appendChild(cardElement);
+        });
 };
 
 function shuffleCards(cards) {
@@ -55,4 +70,27 @@ function shuffleCards(cards) {
             currentIndex--;
             [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];
         };
+};
+
+function flipCard() {
+    this.classList.add("flip");
+}
+
+function createCardContent(card, cardElement) {
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement("div");
+    cardElementFace.classList.add(face);
+        if(face === FRONT) {
+            let iconElement = document.createElement("img");
+            iconElement.classList.add(ICON);
+            iconElement.src = "./assets/images/" + card.icon + ".png";
+            cardElementFace.appendChild(iconElement);
+        } else {
+            cardElementFace.innerHTML = "&lt/&gt";
+        };
+    element.appendChild(cardElementFace);
 };
